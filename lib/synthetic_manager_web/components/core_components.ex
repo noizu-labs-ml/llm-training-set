1c08af39a0e3bea95241ee16ce3a3e6ea455ebca
Keith Brings <keith.brings@noizu.com>
2023-11-14 18:21:40 +0700

    fuxing with css - more or less operable link/header plugin working

--- lib/synthetic_manager_web/components/core_components.ex
+++ lib/synthetic_manager_web/components/core_components.ex
@@ -29,9 +29,11 @@ defmodule SyntheticManagerWeb.CoreComponents do
     """
   end
 
-  def close_menu(assigns) do
+  def open_menu(assigns) do
     ~H"""
-    <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+    <button
+      phx-click={JS.set_attribute({"aria-hidden", "false"}, to: "header .mobile")}
+      type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
       <span class="sr-only">Open main menu</span>
       <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
         <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -132,7 +134,7 @@ defmodule SyntheticManagerWeb.CoreComponents do
           <.logo/>
         </div>
         <div class="flex lg:hidden">
-          <.close_menu/>
+          <.open_menu/>
         </div>
 
 
@@ -144,16 +146,20 @@ defmodule SyntheticManagerWeb.CoreComponents do
         </div>
       </nav>
       <!-- Mobile menu, show/hide based on menu open state. -->
-      <div class="mobile lg:hidden" role="dialog" aria-modal="true">
+      <div class="mobile lg:hidden" role="dialog" aria-modal="true" aria-hidden="true">
         <!-- Background backdrop, show/hide based on slide-over state. -->
         <div class="fixed inset-0 z-10"></div>
-        <div class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div
+          phx-click-away={JS.set_attribute({"aria-hidden", "true"}, to: "header .mobile")}
+          class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Your Company</span>
               <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
             </a>
-            <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+            <button
+              phx-click={JS.set_attribute({"aria-hidden", "true"}, to: "header .mobile")}
+              type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
               <span class="sr-only">Close menu</span>
               <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
